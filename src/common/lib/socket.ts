159314d38c805d3586b4eda81de57f0eb88f9844
Enrique <enriquesamudio1999@gmail.com>
2024-11-14 17:36:29 -0300

    Socket removed

--- src/common/lib/socket.ts
+++
@@ -1,8 +0,0 @@
-import { getEnvVariables } from '../../helpers';
-import { io } from 'socket.io-client';
-
-const { VITE_BACKEND_URL } = getEnvVariables();
-
-const socket = io(VITE_BACKEND_URL);
-
-export default socket;
--- src/context/NotificationsContext.tsx
+++ src/context/NotificationsContext.tsx
@@ -1,8 +1,5 @@
-import { toast } from 'react-toastify';
-import { useAuth } from '../common/hooks';
-import socket from '../common/lib/socket';
 import { INotificationContextType } from '../types';
-import React, { createContext, useContext, useEffect } from 'react';
+import React, { createContext, useContext } from 'react';
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const INITIAL_STATE = {
@@ -12,23 +9,6 @@ export const INITIAL_STATE = {
 const NotificationsContext = createContext<INotificationContextType>(INITIAL_STATE);
 
 const NotificationsProvider = ({ children } : { children: React.ReactNode }) => {
-
-  const { user } = useAuth();
-
-  useEffect(() => {
-    if (user && user.id) {
-      socket.emit('joinRoom', user.id);
-
-      socket.on('newNotification', (notification) => {
-        console.log(notification);
-        toast.success(notification.content);
-      });
-    }
-
-    return () => {
-      socket.off('newNotification');
-    };
-  }, [user]);
 
   const value = {
     test: true
